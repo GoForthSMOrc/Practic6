cad6424f6f9518e49523075eb563990a72267a3e
GoForthSMOrc <79578581+GoForthSMOrc@users.noreply.github.com>
2021-03-19 17:19:58 +0700

    Add files via upload

---
+++ AuthorizationWindow.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Pr6
+{
+    partial class AuthorizationWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.Authorization = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(218, 350);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(145, 88);
+            this.EnterButton.TabIndex = 0;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(12, 350);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(145, 88);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Login.Location = new System.Drawing.Point(53, 166);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(104, 40);
+            this.Login.TabIndex = 2;
+            this.Login.Text = "Login";
+            this.Login.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Password.Location = new System.Drawing.Point(5, 224);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(162, 40);
+            this.Password.TabIndex = 3;
+            this.Password.Text = "Password";
+            // 
+            // Authorization
+            // 
+            this.Authorization.AutoSize = true;
+            this.Authorization.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Authorization.Location = new System.Drawing.Point(74, 48);
+            this.Authorization.Name = "Authorization";
+            this.Authorization.Size = new System.Drawing.Size(225, 40);
+            this.Authorization.TabIndex = 4;
+            this.Authorization.Text = "Authorization";
+            this.Authorization.Click += new System.EventHandler(this.Authorization_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(163, 166);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(200, 40);
+            this.logBox.TabIndex = 5;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(163, 224);
+            this.passBox.Multiline = true;
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(200, 40);
+            this.passBox.TabIndex = 6;
+            this.passBox.UseSystemPasswordChar = true;
+            // 
+            // AuthorizationWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 450);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.Authorization);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.EnterButton);
+            this.Name = "AuthorizationWindow";
+            this.Text = "Authorization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label Authorization;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.TextBox passBox;
+    }
+}
+
